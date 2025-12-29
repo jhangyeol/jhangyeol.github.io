5941fc654b87ee010e2aefc7ef3ddae8fdcf04c3
jhangyeol <hkjung1123@gmail.com>
2025-12-29 08:14:53 +0000

    Deploying to gh-pages from @ jhangyeol/jhangyeol.github.io@5ad8908b62219b8b07e459c02de18fb3a40287ab 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
