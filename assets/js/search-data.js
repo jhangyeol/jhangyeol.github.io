88a311b871165999336a1503a466da7fb7d9112e
jhangyeol <hkjung1123@gmail.com>
2026-07-21 02:45:35 +0000

    Deploying to gh-pages from @ jhangyeol/jhangyeol.github.io@59c874c955b98eaec122745ad6572a2aa657a08e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -64,13 +64,6 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=yGVBIPUAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://kmuhan-study.tistory.com/", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
